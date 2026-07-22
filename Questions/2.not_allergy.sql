80c5adb8f048dd69b4679243152a1da218ccb170
priyanshwho <priyanshu82711@gmail.com>
2026-07-22 17:44:19 +0530

    5Q

---
+++ Questions/2.not_allergy.sql
@@ -0,0 +1,3 @@
+--  Show first name and last name of patients who does not have allergies. (null)
+
+select first_name,last_name from patients where allergies IS NULL;
---
+++ Questions/4.weight(100and120).sql
@@ -0,0 +1,5 @@
+select first_name, last_name from patients where weight >= 100 and weight <= 120;
+
+
+
+select first_name, last_name from patients where weight between 100 and 120;
