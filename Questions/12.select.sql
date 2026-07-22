1cc50782f0ee3707fc037f188f1196b678d3f23b
priyanshwho <priyanshu82711@gmail.com>
2026-07-22 18:40:11 +0530

    easy Q done

---
+++ Questions/12.select.sql
@@ -0,0 +1,8 @@
+-- Show the patient id and the total number of admissions for patient_id 579.
+
+
+
+select patient_id, count(*) as total_admissions from admissions where patient_id=579
+
+select patient_id, count(*) as total_admissions from admissions where patient_id=579 
+group by patient_id
